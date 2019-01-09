ea2e4c5cc8e46d56779780ecd52d7d5012386533
jincl20 <45549791+jincl20@users.noreply.github.com>
2019-01-09 12:23:00 +0800

    排块游戏
    基本游戏功能实现了，需要美化一下

---
+++ 排块游戏/排块游戏/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace 排块游戏
+{
+    partial class 排块游戏
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(1059, 845);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始游戏";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("楷体_GB2312", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(46, 853);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(943, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "点击开始游戏，打乱方块顺序；点击方块可以与周围白块交换位置";
+            // 
+            // 排块游戏
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1381, 911);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "排块游戏";
+            this.Text = "排块游戏";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 排块游戏/排块游戏/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 排块游戏
+{
+    public partial class 排块游戏 : Form
+    {
+        public 排块游戏()
+        {
+            InitializeComponent();
+        }
+        const int n = 4;
+        Button[,] buttons = new Button[n, n];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            generateAllButtons();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            shuffle();
+        }
+        void generateAllButtons()
+        {
+            int x0 = 100, y0 = 10, w = 45, d = 50;
+            for (int r = 0; r < n; r++)
+                for (int c = 0; c < n; c++)
+                {
+                    int num = r * n + c;
+                    Button btn = new Button();
+                    btn.Text = (num + 1).ToString();
+                    btn.Top = y0 + r * d;
+                    btn.Left = x0 + c * d;
+                    btn.Width = w;
+                    btn.Height = w;
+                    btn.Visible = true;
+                    btn.Tag = r * n + c;
+
+                    //btn.Click +=   new EventHandler(Btn_Click);
+                    btn.Click += Btn_Click;
+
+                    buttons[r, c] = btn;
+                    this.Controls.Add(btn);
+                }
+            buttons[n - 1, n - 1].Visible = false;
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            Button blank = Findhiddenbutton();//查找隐藏按钮
+
+            //判断是否和空白块相邻，是的话就交换
+            if(IsNeighbor(btn,blank ))
+            {
+                swap(btn, blank);
+                blank.Focus();
+            }
+            //判断是否完成了
+            if(ResultIsOk())
+            {
+                MessageBox.Show("ok");
+            }
+        }
+
+        //查找隐藏按钮
+        Button Findhiddenbutton()
+        {
+            for(int r=0;r<n;r++)
+                for(int c=0;c<n;c++)
+                {
+                    if(!buttons[r,c ].Visible)
+                    {
+                        return buttons[r, c];
+                    }
+                }
+            return null;
+        }
+        //判断是否相邻
+        bool IsNeighbor(Button btna,Button btnb)
+        {
+            int a = (int)btna.Tag;// btn.Tag = r * n + c;
+            int b = (int)btnb.Tag;
+            int r1 = a / n, c1 = a % n;
+            int r2 = b / n, c2 = b % n;
+
+            if (r1 == r2 && (c1 == c2 - 1 || c1 == c2 + 1) || c1 == c2 && (r1 == r2 - 1 || r1 == r2 + 1))
+                return true;
+            return false;
+
+        }
+        //判断是否完成
+        bool ResultIsOk()
+        {
+            for (int r = 0; r < n; r++)
+                for (int c = 0; c < n; c++)
+                {
+                    if (buttons[r,c].Text !=(r*n+c+1).ToString())
+                    {
+                        return false;
+                    }
+                }
+            return true;
+        }
+        //多次随机打乱按钮次序
+        void shuffle()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                int a = rnd.Next(n);
+                int b = rnd.Next(n);
+                int c = rnd.Next(n);
+                int d = rnd.Next(n);
+                swap(buttons[a, b], buttons[c, d]);
+            }
+
+
+        }
+
+        void swap(Button btna ,Button btnb)
+        {
+            string t = btna.Text;
+            btna.Text = btnb.Text;
+            btnb.Text = t;
+
+            bool v = btna.Visible;
+            btna.Visible = btnb.Visible;
+            btnb.Visible = v;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
